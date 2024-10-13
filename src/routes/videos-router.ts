8fa5f80bc623fb080c0c2f1c8bff34b9af1182ce
news1d <news1dee@yandex.ru>
2024-10-14 00:30:27 +0300

    Added trim() function to some checks and fixed work with HTTP statuses

--- src/routes/videos-router.ts
+++ src/routes/videos-router.ts
@@ -22,14 +22,14 @@ videosRouter.post('/', (req: RequestWithBody<CreateVideoInputModel>,
 
     const errors : FieldError[] = [];
 
-    if (!req.body.title || req.body.title.length > 40) {
+    if (!req.body.title.trim() || req.body.title.trim().length > 40) {
         errors.push({
             message: "The value is missing or the maximum allowed size has been exceeded",
             field: 'title'
         });
     }
 
-    if (!req.body.author || req.body.author.length > 20) {
+    if (!req.body.author.trim() || req.body.author.trim().length > 20) {
         errors.push({
             message: "The value is missing or the maximum allowed size has been exceeded",
             field: 'author'
@@ -77,10 +77,10 @@ videosRouter.get('/:id', (req: RequestWithParams<URIParamsVideoIdModel>, res: Re
     const foundVideo = findVideoById(+req.params.id)
 
     if (!foundVideo) {
-        res.sendStatus(404)
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
         return;
     }
-    res.status(200).json(foundVideo)
+    res.status(HTTP_STATUSES.OK_200).json(foundVideo)
 
 })
 
@@ -97,14 +97,14 @@ videosRouter.put('/:id', (req: RequestWithParamsAndBody<URIParamsVideoIdModel, U
     const errors : FieldError[] = [];
 
     // Проверка на наличие title и author, а также их максимальную длину
-    if (!req.body.title || req.body.title.length > 40) {
+    if (!req.body.title.trim() || req.body.title.trim().length > 40) {
         errors.push({
             message: 'The value is missing or the maximum allowed size has been exceeded',
             field: 'title'
         });
     }
 
-    if (!req.body.author || req.body.author.length > 20) {
+    if (!req.body.author.trim() || req.body.author.trim().length > 20) {
         errors.push({
             message: 'The value is missing or the maximum allowed size has been exceeded',
             field: 'author'
